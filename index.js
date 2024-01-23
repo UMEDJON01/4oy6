eb9bee0584855ca24420f09cc88b6c249e570af6
umedjon01 <najottalimfn15@gmail.com>
2024-01-23 21:20:33 +0500

    Homework

---
+++ index.js
@@ -0,0 +1,70 @@
+//  1-chi masala
+
+// let n = 123;
+// let result = "";
+
+// switch (n) {
+//   case 1:
+//     result = "Yanvar";
+//     break;
+//   case 2:
+//     result = "Fevral";
+//     break;
+//   case 3:
+//     result = "Mart";
+//     break;
+//   case 4:
+//     result = "Aprel";
+//     break;
+//   case 5:
+//     result = "May";
+//     break;
+//   case 6:
+//     result = "Iyun";
+//     break;
+//   case 7:
+//     result = "Iyul";
+//     break;
+//   case 8:
+//     result = "Avgust";
+//     break;
+//   case 9:
+//     result = "Sentabr";
+//     break;
+//   case 10:
+//     result = "Oktabr";
+//     break;
+//   case 11:
+//     result = "Noyabr";
+//     break;
+//   case 12:
+//     result = "Dekabr";
+//     break;
+//   default:
+//     result = "Bunday oy yuq";
+//     break;
+// }
+// console.log(result);
+
+//  2-chi masala
+
+// let a = 1;
+// if (a == 1 || a == 2 || a == 12) {
+//   console.log("Bu qish faslidagi oy");
+// } else if (a >= 3 && a <= 5) {
+//   console.log("Bu Bahor faslidagi oy");
+// } else if (a > 5 && a <= 8) {
+//   console.log("Bu Yoz faslidagi oy");
+// } else if (a > 8 && a <= 11) {
+//   console.log("Bu Kuz faslidagi oy");
+// } else {
+// console.log("Bunday oy yuq");
+//}
+
+//  5-chi masala
+
+// let n = +prompt("Son kiriting");
+// let a = 1;
+// for (let i = n; a <= i; i--) {
+//   console.log(i);
+// }
